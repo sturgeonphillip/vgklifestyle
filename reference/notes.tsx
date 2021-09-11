8922fd126606f9f36dd6ff3fe4335176d60df6f4
Phillip Sturgeon <sturgeonphillip@gmail.com>
2021-09-10 20:49:23 -0700

    parsing individual cards

---
+++ reference/notes.tsx
@@ -0,0 +1,53 @@
+// full computer screen size (for me): 
+// zoom out: 4-5 columns
+// smaller window, 3 columns, then 2 (larger, but side by side aligned)
+// finally, smallest is one card that makes it look like an instagram feed
+
+// They're all just cards of differing portions that link to an article or story from a social media account post. Which means For each card there are the same repeatable properties (modular components--build 1 -3 sizes).
+
+const card: object = {
+  "Article": {
+    "properties": {
+      'classes': [9, '...'],
+      'id': 'article-###',
+      'label': 'Blog Post',
+      'data-item-id': 'a#',
+      'data-offset': '###',
+      'style': 'HAND STYLED PROPERTIES',
+    },
+    'Div': {
+      'properties': {
+        'classes': 'thumbnail-title-wrapper',
+      },
+      'Div': {
+        'properties': {
+          'classes': 'excerpt-thumb',
+        },
+        'A': {
+          'properties': {
+            'classes': [3, '...'],
+            'style': 'HAND STYLED PROPERTIES'
+          },
+          'Img': [
+            'data-src', '...src',
+          ],
+          'Div': {
+            'classes': 'thumb-overlay',
+          }
+        }
+      }
+    },
+    'Div': 'entry-text' => 'header' => ??;
+  }
+}
+{
+
+
+},
+{
+
+}
+  ] 
+    
+    
+}
